55fb9e2351234275ac336856e6eaca7a1dcc59b7
ArteMonster3228 <60046108+ArteMonster3228@users.noreply.github.com>
2020-06-01 14:59:18 +0300

    Update

--- coursework.py
+++ coursework.py
@@ -14,7 +14,7 @@ import math
 import matplotlib.pyplot as plt
 
 
-_lambda = int(input("Lambda: "))
+_lambda = int(input("Intensity: "))
 time_units = int(input("Number of time units: "))
 
 _event_num = []
@@ -24,7 +24,7 @@ _event_time = 0
 t = 0
 i = 1
 
-print('Number of event' + '  |      ' + 'Event time' + '      |   ' + 'Absolute event time')
+print('Index of event' + '  |      ' + 'Time interval' + '      |   ' + 'Absolute event time')
 
 while t < time_units:
   n = random.random()
@@ -37,31 +37,25 @@ while t < time_units:
     _inter_event_times.append(_inter_event_time)
     _event_time = _event_time + _inter_event_time
     _event_times.append(_event_time)
-    print('      ' + str(i)[:16] +'               ' + str(_inter_event_time)[:16] + '           ' + str(_event_time))
+    print('      ' + str(i)[:10] +'               ' + str(_inter_event_time)[:10] + '               ' + str(_event_time)[:10])
     i = i + 1
 
-fig = plt.figure()
-fig.suptitle('Times between consecutive events')
-plot, = plt.plot(_event_num, _inter_event_times, 'bo-', label='Inter-event time')
-plt.legend(handles=[plot])
-plt.xlabel('Index of event')
-plt.ylabel('Time')
-plt.show()
+y = [0] + _event_num
+x = _event_times + [time_units]
 
-fig = plt.figure()
-fig.suptitle('Absolute times of consecutive events')
-plot, = plt.plot(_event_num, _event_times, 'bo-', label='Absolute time of event')
-plt.legend(handles=[plot])
-plt.xlabel('Index of event')
-plt.ylabel('Time')
-plt.show()
+fig = plt.figure(figsize=(20,10))
+plt.step(x, y)
+plt.suptitle('Time')
+plt.xlabel('Actual time')
+plt.ylabel('Index of event')
+plt.plot(x, y, color='red', marker='o', markersize=2, linewidth=0)
 
 _interval_nums = []
 _num_events_in_interval = []
 _interval_num = 1
 _num_events = 0
 
-print('Interval number  |   Number of events in interval')
+print('Interval index  |   Number of events in interval')
 
 for i in range(len(_event_times)):
 	_event_time = _event_times[i]
@@ -75,7 +69,7 @@ for i in range(len(_event_times)):
 		_num_events = 1
 
 fig = plt.figure()
-fig.suptitle('Number of events occurring in consecutive intervals')
+fig.suptitle('Number of events occurring in consecutive interval')
 plt.bar(_interval_nums, _num_events_in_interval)
 plt.xlabel('Index of interval')
 plt.ylabel('Number of events')
@@ -112,23 +106,24 @@ while t < 1:
     _inter_event_times.append(_inter_event_time)
     _event_time = _event_time + _inter_event_time
     _event_times.append(_event_time)
-    print('      ' + str(i)[:16] +'                       ' + str(_inter_event_time)[:16] + '                     ' + str(_event_time) + '                 ' + str(fans))
+    print('      ' + str(i)[:10] +'                       ' + str(_inter_event_time)[:10] + '                             ' + str(_event_time)[:10] + '                 ' + str(fans))
     i = i + 1
 
-fig = plt.figure()
-fig.suptitle('Times between consecutive events')
-plot, = plt.plot(_event_num, _inter_event_times, 'bo-', label='Inter-event time')
-plt.legend(handles=[plot])
-plt.xlabel('Index of event')
-plt.ylabel('Time')
-plt.show()
+y = [0] + _event_num
+x = _event_times + [1]
+
+fig = plt.figure(figsize=(20,10))
+plt.step(x, y)
+plt.suptitle('Time')
+plt.xlabel('Actual time')
+plt.ylabel('Index of event')
+plt.plot(x, y, color='red', marker='o', markersize=2, linewidth=0)
 
 fig = plt.figure()
-fig.suptitle('Absolute times of consecutive events')
-plot, = plt.plot(_event_num, _event_times, 'bo-', label='Absolute time of event')
-plt.legend(handles=[plot])
-plt.xlabel('Index of event')
-plt.ylabel('Time')
+fig.suptitle('Quantity of fans in consecutive buss')
+plt.bar(_event_num, fans_quantity)
+plt.xlabel('Index of interval')
+plt.ylabel('Number of events')
 plt.show()
 
 print('Total quantity of fans: ', sum(fans_quantity))
@@ -145,7 +140,7 @@ _event_times = []
 _event_time = 0
 i = 1
 
-_lambda = int(input("Lambda(should be 7 or higher): "))
+_lambda = 7
 time_units = 10
 
 print('Number of event' + '   |   ' + 'Time diff between consecutive events' + '   |   ' + 'Absolute event time')
@@ -163,24 +158,18 @@ while _event_time < time_units:
       _event_num.append(i)
       _inter_event_times.append(_inter_event_time)
       _event_times.append(_event_time)
-      print('      ' + str(i)[:16] +'                       ' + str(_inter_event_time)[:16] + '                     ' + str(_event_time))
+      print('      ' + str(i)[:10] +'                       ' + str(_inter_event_time)[:10] + '                          ' + str(_event_time)[:10])
       i = i + 1
 
-fig = plt.figure()
-fig.suptitle('Times between consecutive events')
-plot, = plt.plot(_event_num, _inter_event_times, 'bo-', label='Inter-event time')
-plt.legend(handles=[plot])
-plt.xlabel('Index of event')
-plt.ylabel('Time')
-plt.show()
+y = [0] + _event_num
+x = _event_times + [time_units]
 
-fig = plt.figure()
-fig.suptitle('Absolute times of consecutive events')
-plot, = plt.plot(_event_num, _event_times, 'bo-', label='Absolute time of event')
-plt.legend(handles=[plot])
-plt.xlabel('Index of event')
-plt.ylabel('Time')
-plt.show()
+fig = plt.figure(figsize=(20,10))
+plt.step(x, y)
+plt.suptitle('Time')
+plt.xlabel('Actual time')
+plt.ylabel('Index of event')
+plt.plot(x, y, color='red', marker='o', markersize=2, linewidth=0)
 
 _interval_nums = []
 _num_events_in_interval = []
@@ -205,6 +194,7 @@ fig.suptitle('Number of events occurring in consecutive intervals')
 plt.bar(_interval_nums, _num_events_in_interval)
 plt.xlabel('Index of interval')
 plt.ylabel('Number of events')
+
 plt.show()
 
 """**Fourth task**"""
@@ -217,51 +207,52 @@ _event_num = []
 _inter_event_times = []
 _event_times = []
 _event_time = 0
+_total_time_interval = 0
 time_units = 10
 i = 1
+_lambda = 1
+current = 5
 
-_lambda = int(input("Lambda(should be 26 or higher): "))
-
-print('Number of event' + '   |   ' + 'Time diff between consecutive events' + '   |   ' + 'Absolute event time')
+print('Number of event' + '      |      ' + 'Time interval' + '      |      ' + 'Absolute event time')
 
 while _event_time < time_units:
   n = random.random()
   _inter_event_time = -math.log(1.0 - n) / _lambda
+  if _event_time + _inter_event_time > current:
+    _inter_event_time = (_inter_event_time + _event_time - current ) * (_lambda/(_lambda + 5))
+    current += 1
+    if current == 11:
+      break
+    _lambda += 5
   _event_time = _event_time + _inter_event_time
-  if 0 < _event_time < 5 :
-    func = _event_time/5
-    u = random.random()
-    if u <= func:
-      _event_num.append(i)
-      _inter_event_times.append(_inter_event_time)
+  _total_time_interval = _total_time_interval + _inter_event_time 
+  n = random.random()
+  if current == 5:
+    if n <= (_event_time/5)/_lambda:
+      _event_num.append(i + 1)
+      _inter_event_times.append(_total_time_interval)
       _event_times.append(_event_time)
-      print('      ' + str(i)[:16] +'                       ' + str(_inter_event_time)[:16] + '                     ' + str(_event_time))
-      i = i + 1 
-  elif 5 < _event_time < 10 :
-    func = 1 + 5*(_event_time - 5)
-    u = random.random()
-    if u <= func:
+      print('      ' + str(i) +'                       ' + str(_total_time_interval)[:10] + '               ' + str(_event_time)[:10])
+      i += 1
+      _total_time_interval = 0
+  elif current > 5:
+    if n <= (1 + 5*(_event_time - 5))/_lambda:
       _event_num.append(i + 1)
       _inter_event_times.append(_inter_event_time)
       _event_times.append(_event_time)
-      print('      ' + str(i)[:16] +'                       ' + str(_inter_event_time)[:16] + '                     ' + str(_event_time))
-      i = i + 1
+      print('      ' + str(i) +'                       ' + str(_total_time_interval)[:10] + '               ' + str(_event_time)[:10])
+      i += 1
+      _total_time_interval = 0
 
-fig = plt.figure()
-fig.suptitle('Times between consecutive events')
-plot, = plt.plot(_event_num, _inter_event_times, 'bo-', label='Inter-event time')
-plt.legend(handles=[plot])
-plt.xlabel('Index of event')
-plt.ylabel('Time')
-plt.show()
+y = [0] + _event_num
+x = _event_times + [time_units]
 
-fig = plt.figure()
-fig.suptitle('Absolute times of consecutive events')
-plot, = plt.plot(_event_num, _event_times, 'bo-', label='Absolute time of event')
-plt.legend(handles=[plot])
-plt.xlabel('Index of event')
-plt.ylabel('Time')
-plt.show()
+fig = plt.figure(figsize=(30,15))
+plt.step(x, y)
+plt.suptitle('Time')
+plt.xlabel('Actual time')
+plt.ylabel('Index of event')
+plt.plot(x, y, color='red', marker='o', markersize=2, linewidth=0)
 
 _interval_nums = []
 _num_events_in_interval = []
